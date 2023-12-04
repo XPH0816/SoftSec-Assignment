d3fc333ffe9ec1a9b42374a225cf5942a4e1d61a
Lim Shi Song <limshisong123@gmail.com>
2023-12-04 22:05:53 +0800

    Add user input validation functions and update main program logic

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-typedef char * string;
-
+#include "utils/cli.h"
 typedef struct USER
 {
     string name;
@@ -12,35 +13,165 @@ typedef struct USER
     int code;
 } User;
 
-int getValidInt(int min, int max)
+bool checkBirth(string str)
 {
-    int input;
-    do
+    int day, month, year;
+    day = strtol(str, &str, 10);
+    if (*str != '/')
     {
-        scanf("%d", &input);
-        if (input < min || input > max)
-        {
-            printf("Invalid input, please try again: ");
-        }
-    } while (input < min || input > max);
-    return input;
+        return false;
+    }
+    month = strtol(++str, &str, 10);
+    if (*str != '/')
+    {
+        return false;
+    }
+    year = strtol(++str, &str, 10);
+    if (month <= 8 && month % 2 == 1 && day > 31)
+    {
+        return false;
+    }
+    if (month <= 8 && month % 2 == 0 && day > 30)
+    {
+        return false;
+    }
+    if (month > 8 && month % 2 == 1 && day > 30)
+    {
+        return false;
+    }
+    if (month > 8 && month % 2 == 0 && day > 31)
+    {
+        return false;
+    }
+    if (month == 2 && year % 4 == 0 && day > 29)
+    {
+        return false;
+    }
+    if (month == 2 && year % 4 != 0 && day > 28)
+    {
+        return false;
+    }
+    return true;
 }
 
-bool checkLength(string str, int length)
+bool checkPassport(string str)
 {
-    int count = 0;
-    while (str[count] != '\0')
-    {
-        count++;
+    if(strlen(str) == 9 && checkDigits(str + 2, 7)) {
+        return true;
     }
-    if (count == length)
+    if (strlen(str) == 10 && checkDigits(str + 2, 8))
     {
         return true;
     }
     return false;
 }
 
+string getValidPassport()
+{
+    string passport;
+    do
+    {
+        passport = getValidString(11);
+        if (!checkUpperCaseAlphabet(passport[0]) || !checkUpperCaseAlphabet(passport[1]))
+        {
+            printf("First and 2nd character must be uppercase alphabet, please try again: ");
+        }
+        if (!checkPassport(passport))
+        {
+            printf("Last 7 or 8 characters must be digits, please try again: ");
+        }
+    } while (!checkUpperCaseAlphabet(passport[0]) || !checkUpperCaseAlphabet(passport[1]) || !checkPassport(passport));
+    return passport;
+}
+
+string getValidName()
+{
+    string name;
+    do
+    {
+        name = getValidString(50);
+        if (!checkOnlyString(name))
+        {
+            printf("Only alphabet is allowed, please try again: ");
+        }
+    } while (!checkOnlyString(name));
+    return name;
+}
+
+string getValidDOB()
+{
+    string DOB;
+    do
+    {
+        DOB = getValidString(12);
+        // why 12? dd/mm/yyyy is 10 characters,
+        // but we need to add 2 more characters for the '\0' and the '\n' from fgets()
+        if (!checkBirth(DOB))
+        {
+            printf("Invalid date, please try again: ");
+        }
+    } while (!checkBirth(DOB));
+    return DOB;
+}
+
+string getValidCountry()
+{
+    string country;
+    do
+    {
+        country = getValidString(50);
+        if (!checkOnlyString(country))
+        {
+            printf("Only alphabet is allowed, please try again: ");
+        }
+    } while (!checkOnlyString(country));
+    return country;
+}
+
+User *getUser()
+{
+    User *user = (User *)malloc(sizeof(User));
+    printf("Enter name: ");
+    user->name = getValidName();
+    printf("Enter date of birth: ");
+    user->DOB = getValidDOB();
+    printf("Enter passport: ");
+    user->passport = getValidPassport();
+    printf("Enter country: ");
+    user->country = getValidCountry();
+    printf("Enter code (1: BIT, 2: BIS, 3: BIP): ");
+    user->code = getValidInt(1, 3);
+    return user;
+}
+
+void printProgramCode(int code)
+{
+    printf("Program Name: ");
+    switch (code)
+    {
+    case 1:
+        printf(" BIT\n");
+        break;
+    case 2:
+        printf(" BIS\n");
+        break;
+    case 3:
+        printf(" BIP\n");
+        break;
+    }
+}
+
 int main()
 {
+    User *user = getUser();
+    clearScreen();
+    printf("User information:\n");
+    printf("====================================\n");
+    printf("Name: %s\n", user->name);
+    printf("Date of birth: %s\n", user->DOB);
+    printf("Passport: %s\n", user->passport);
+    printf("Country: %s\n", user->country);
+    printf("Code: %d\n", user->code);
+    printProgramCode(user->code);
     return 0;
 }
